178dd840c3b843763548eb8245673ca98eb2ee99
davi <davi@okn.com.br>
2026-05-06 09:26:27 -0300

    arrumando o projeto

--- Project2/Controllers/HomeController.cs
+++ Project2/Controllers/HomeController.cs
@@ -1,21 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
+using Project2.CarrinhoCompra;
 using Project2.Models;
+using Project2.Repository;
+using Project2.Repository.Contract;
 using System.Diagnostics;
 
 namespace Project2.Controllers
 {
     public class HomeController : Controller
     {
+        private ILivroRepository _livroRepository;
+        private CookieCarrinhoCompra _cookieCarrinhoCompra;
+
+        private IEmprestimoRepository _emprestimoRepository;
+        private IItemRepository _itemRepository;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController( ILivroRepository livroRepository,CookieCarrinhoCompra cookieCarrinhoCompra,
+                               IEmprestimoRepository emprestimoRepository,IItemRepository itemRepository)
         {
-            _logger = logger;
+            _livroRepository = livroRepository;
+            _cookieCarrinhoCompra = cookieCarrinhoCompra;
+            _emprestimoRepository = emprestimoRepository;
+            _itemRepository = itemRepository;
         }
-
+}
         public IActionResult Index()
         {
-            return View();
+            return View(_livroRepository.ObterTodosLivros());
         }
 
         public IActionResult Privacy()
--- Project2/obj/Debug/net8.0/Project2.AssemblyInfo.cs
+++ Project2/obj/Debug/net8.0/Project2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+406ee2910e19bccf882773c786ba6ddfb6164a8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11568932ff8a959311aef717bfa3152633de8c7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
